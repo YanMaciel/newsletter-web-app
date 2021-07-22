55e4b736601d0ada9073cdc9841e8582be83c924
Yan Maciel <30478786+YanMaciel@users.noreply.github.com>
2021-07-22 12:02:45 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,71 @@
+// Newsletter Signup Project using node.js, express.js and mailchimp's server via API
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html");
+})
+
+app.post("/", function(req, res){
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    // Getting config vars from Heroku.
+    const LISTID = process.env.LISTID;
+    const KEY = process.env.KEY;
+
+    const url = "https://us6.api.mailchimp.com/3.0/lists/" + LISTID;
+
+    const options = {
+        method: "POST",
+        auth: "YanMaciel:" + KEY
+    }
+
+    const request = https.request(url, options, function(response){
+        
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+        
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    });
+    request.write(jsonData);
+    request.end();
+});
+
+app.post("/failure", function(req, res){
+    res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Server is running on port 3000.");
+})
